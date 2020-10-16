a1c9bff4a30f3eaa9a9a2dd12857ea4fe54f37a8
Adomas Gaudiesius <adomas.gaudi@gmail.com>
2020-10-17 00:19:14 +0300

    up

--- js/app.js
+++ js/app.js
@@ -1,18 +1,27 @@
-alert();
+// alert();
 // Check if a new cache is available on page load.
-window.addEventListener('load', function(e) {
+// window.addEventListener('load', function(e) {
 
-    window.applicationCache.addEventListener('updateready', function(e) {
-      if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
-        // Browser downloaded a new app cache.
-        // Swap it in and reload the page to get the new hotness.
-        window.applicationCache.swapCache();
-        if (confirm('A new version of this site is available. Load it?')) {
-          window.location.reload();
-        }
-      } else {
-        // Manifest didn't changed. Nothing new to server.
-      }
-    }, false);
+//     window.applicationCache.addEventListener('updateready', function(e) {
+//       if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+//         // Browser downloaded a new app cache.
+//         // Swap it in and reload the page to get the new hotness.
+//         window.applicationCache.swapCache();
+//         if (confirm('A new version of this site is available. Load it?')) {
+//           window.location.reload();
+//         }
+//       } else {
+//         // Manifest didn't changed. Nothing new to server.
+//       }
+//     }, false);
   
-  }, false);
+//   }, false);
+
+
+    // function runbtn(){
+    //     console.log("alert");
+    //   alert("console.log")
+    // }
+    console.log(document.getElementById("signupbtn"))
+
+    
