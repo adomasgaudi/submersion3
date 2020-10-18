d71bdf615dd351d689f9f86ec8a5c822d5c5b502
Adomas Gaudiesius <adomas.gaudi@gmail.com>
2020-10-19 01:01:59 +0300

    up

--- js/firebase.js
+++ js/firebase.js
@@ -29,5 +29,160 @@
 
 
 
- 
+ ////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+ console.log("FIREBASE JS running");
+
+
+const id = (name) => { return document.getElementById(name) }
+
+const userlist = document.querySelector("#userlist");
+const form_adduser = document.querySelector("#add-user-form")
+const form_login = document.querySelector("#login-form")
+
+function findattr(attribute, value, element_type)    {
+    element_type = element_type || "*";
+
+    var All = document.getElementsByTagName(element_type);
+    for (var i = 0; i < All.length; i++)       {
+      if (All[i].getAttribute(attribute) == value) { return All[i]; }
+    }
+    
+  }
+
+
+function renderuser(doc) {
+    // creates elements
+    let lidoc = document.createElement("li");
+    let namespan = document.createElement("span");
+    let paswspan = document.createElement("span");
+    let scorespan = document.createElement("span");
+    let cross = document.createElement("div");
+
+    //future
+    lidoc.setAttribute("data-id", doc.id);
+
+    //fills elements
+    namespan.textContent = "name: " + doc.data().name + " " ;
+    namespan.setAttribute("name", doc.data().name )
+    paswspan.textContent = "pasw: " + doc.data().pasw + " " ;
+    scorespan.textContent = "score: " + doc.data().score + " " ; 
+    cross.textContent = "delete";
+    cross.classList += "button";
+
+    lidoc.appendChild(namespan);
+    lidoc.appendChild(paswspan);
+    lidoc.appendChild(scorespan);
+    lidoc.appendChild(cross)
+
+    userlist.appendChild(lidoc)
+
+
+    cross.addEventListener("click", (e)=>{
+        e.stopPropagation();
+        let id = e.target.parentElement.getAttribute("data-id");
+        db.collection("users").doc(id).delete();
+
+        console.log("deleted?", db.collection("users").doc(id) )
+        console.log(    "if this works...",  db.collection('users').doc(id).get().then((doc) => {doc.get("name")})        );
+    })
+
+
+
+
+    
+}
+
+
+
+db.collection("users").get().then((snapshot)=>{
+    // console.log(snapshot.docs)
+    snapshot.docs.forEach( doc => {
+        console.log(doc.data());
+        renderuser(doc);
+    } )
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+//FORMS
+
+//ADD USER
+form_adduser.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    db.collection("users").add({
+        name: form_adduser.name.value,
+        pasw: form_adduser.pasw.value,
+        score: "0"
+
+    })
+    form_adduser.name.value = "";
+    form_adduser.pasw.value = "";
+})
+
+
+
+// LOGIN
+    form_login.addEventListener("submit", (e)=>{
+        e.preventDefault();
+    
+        // console.log("login: ", form_login.name.value, findattr( "name", form_login.name.value, "span"));
+        let element = findattr( "name", form_login.name.value, "span" )
+        let id = element.parentElement.getAttribute("data-id");
+        console.log("id", id);
+
+        db.collection("users").where("name", "==", "alex").get().then((snapshot)=>{
+                snapshot.docs.forEach(doc => {
+                    let pasw = doc.data().pasw
+                    document.getElementById("user").textContent = pasw;
+                })
+        })
+        
+    
+    });
+
+
+
+
+
+//POP UP SIGN UP
+    
+    function dissapear(){
+        document.getElementById('id01').style.display='none';
+        console.log("ran dissapear")
+    }
+
+    function reappear(){
+        document.getElementById('id01').style.display='block';
+        console.log("ran reappear")
+    }
+
+    // Get the modal
+  var modal = document.getElementById('id01');
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
     
