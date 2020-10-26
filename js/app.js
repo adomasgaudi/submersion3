51c56e3ad14c581f34bf3c04dcd29ad07571a9a9
Adomas <adomas.gaudi@gmail.com>
2020-10-26 16:38:16 +0200

    cards

--- js/app.js
+++ js/app.js
@@ -94,108 +94,58 @@ let fakearray = [
 
 
 
-  $(".expTop").on("click", function(){
-    console.log($(this).next().css("display"))
-    var content = $(this).next();
-    if (content.css("display") == "none") {      
-      content.css("display","block");    } 
-    else {content.css("display","none")}
-  });
-
-
-  $(".popupTop").on("click", function(){
-    var content = $(this).parent().nextAll(".popupBox").eq( 0 );
-    if (content.css("display") == "none") {      
-      content.css("display","block");    } 
-    else {content.css("display","none")}
-  });
-
-  $(".popupTop").on("click",function(){
-    $(this).css("color","maroon")
-  })
-  // $(".popupTop:hover").css("color","red")
-  
+$(".expTop").on("click", function(){
+  console.log($(this).next().css("display"))
+  var content = $(this).next();
+  if (content.css("display") == "none") {      
+    content.css("display","block");    } 
+  else {content.css("display","none")}
+});
 
 
 
 
+$(".popupTop").on("click", function(){
+  var content = $(this).parent().nextAll(".popupBot").eq( 0 );
+  if (content.css("display") == "none") {      
+    content.css("display","block");    } 
+  else {content.css("display","none")}
+});
 
-  // Vue.config.devtools = true;
+$(".popupTop2").on("click", function(){
+  var content = $(this).parent().nextAll(".popupBot2").eq( 0 );
+  if (content.css("display") == "none") {      
+    content.css("display","block");    } 
+  else {content.css("display","none")}
+});
 
-  // Vue.component('card', {
-  //   template: `
-  //     <div class="card-wrap"
-  //       @mousemove="handleMouseMove"
-  //       @mouseenter="handleMouseEnter"
-  //       @mouseleave="handleMouseLeave"
-  //       ref="card">
-  //       <div class="card"
-  //         :style="cardStyle">
-  //         <div class="card-bg" :style="[cardBgTransform, cardBgImage]"></div>
-  //         <div class="card-info">
-  //           <slot name="header"></slot>
-  //           <slot name="content"></slot>
-  //         </div>
-  //       </div>
-  //     </div>`,
-  //   mounted() {
-  //     this.width = this.$refs.card.offsetWidth;
-  //     this.height = this.$refs.card.offsetHeight;
-  //   },
-  //   props: ['dataImage'],
-  //   data: () => ({
-  //     width: 0,
-  //     height: 0,
-  //     mouseX: 0,
-  //     mouseY: 0,
-  //     mouseLeaveDelay: null }),
-  
-  //   computed: {
-  //     mousePX() {
-  //       return this.mouseX / this.width;
-  //     },
-  //     mousePY() {
-  //       return this.mouseY / this.height;
-  //     },
-  //     cardStyle() {
-  //       const rX = this.mousePX * 30;
-  //       const rY = this.mousePY * -30;
-  //       return {
-  //         transform: `rotateY(${rX}deg) rotateX(${rY}deg)` };
-  
-  //     },
-  //     cardBgTransform() {
-  //       const tX = this.mousePX * -40;
-  //       const tY = this.mousePY * -40;
-  //       return {
-  //         transform: `translateX(${tX}px) translateY(${tY}px)` };
-  
-  //     },
-  //     cardBgImage() {
-  //       return {
-  //         backgroundImage: `url(${this.dataImage})` };
-  
-  //     } },
-  
-  //   methods: {
-  //     handleMouseMove(e) {
-  //       this.mouseX = e.pageX - this.$refs.card.offsetLeft - this.width / 2;
-  //       this.mouseY = e.pageY - this.$refs.card.offsetTop - this.height / 2;
-  //     },
-  //     handleMouseEnter() {
-  //       clearTimeout(this.mouseLeaveDelay);
-  //     },
-  //     handleMouseLeave() {
-  //       this.mouseLeaveDelay = setTimeout(() => {
-  //         this.mouseX = 0;
-  //         this.mouseY = 0;
-  //       }, 1000);
-  //     } } });
-  
-  
-  
-  // const app = new Vue({
-  //   el: '#app' });
+
+$(".popupTop").on("click",function(){
+  $(this).css("color","maroon")
+})
+$(".popupTop2").on("click",function(){
+  $(this).css("color","maroon")
+})
+// $(".popupTop:hover").css("color","red")
+
+
+
+
+// $(".artTop").on("click", function(){
+//   var content = $(this).nextAll(".artBot").eq( 0 );
+//   if (content.css("display") == "none") {      
+//     content.css("display","block");    } 
+//   else {content.css("display","none")}
+// });
+
+$(".artTop").on("click", function(){
+  var content = $(this).nextAll(".artBot").eq( 0 );
+  if (content.css("display") == "none") {      
+    content.slideDown()    } 
+  else {content.slideUp()}
+});
+
+
 
 
 
