0b09400ed1d1380c9741df1146f51be6cfe1011d
Adomas <adomas.gaudi@gmail.com>
2020-11-20 11:48:42 +0200

    dev mode

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 
 
-// AJAX html INCLUDES
+// AJAX html INCLUDES from css tricks
+
 fetch("./htmls/footer.html")
   .then(response => {
     return response.text()
@@ -8,3 +9,61 @@ fetch("./htmls/footer.html")
   .then(data => {
     document.querySelector("footer").innerHTML = data;
   });
+
+
+
+
+
+
+
+
+
+    
+
+
+
+(function (global) {
+  var dc = {};
+  
+  var ankiHtml = "./htmls/ankitut.html";
+
+
+  const insertHtml = (selector, html)=>{
+    let element = document.querySelector(selector);
+    element.innerHTML = html;
+  }
+
+  dc.getHtml = function () {
+    console.log(  );
+    $ajaxUtils.sendGetRequest(
+      ankiHtml,
+      function (receivedHtml) {
+        console.log(receivedHtml, "inserted HTML yay!!!!!!!!!")
+        insertHtml("#ankiExplained",receivedHtml);
+
+      },
+      false);
+
+  }
+
+
+
+
+
+ 
+
+
+  dc.devopen = function () {
+    document.querySelector("#devBtn").style.display = "inline";
+  }
+
+  dc.devTog = function () {
+    $(".devMode").css("display", "block")
+    $(".col1").css("background", "crimson")
+  }
+
+
+
+  
+  global.$dc = dc;
+  })(window);
--- js/dist/app.dev.js
+++ js/dist/app.dev.js
@@ -1,8 +1,37 @@
 "use strict";
 
-// AJAX html INCLUDES
+// AJAX html INCLUDES from css tricks
 fetch("./htmls/footer.html").then(function (response) {
   return response.text();
 }).then(function (data) {
   document.querySelector("footer").innerHTML = data;
 });
+
+(function (global) {
+  var dc = {};
+  var ankiHtml = "./htmls/ankitut.html";
+
+  var insertHtml = function insertHtml(selector, html) {
+    var element = document.querySelector(selector);
+    element.innerHTML = html;
+  };
+
+  dc.getHtml = function () {
+    console.log();
+    $ajaxUtils.sendGetRequest(ankiHtml, function (receivedHtml) {
+      console.log(receivedHtml, "inserted HTML yay!!!!!!!!!");
+      insertHtml("#ankiExplained", receivedHtml);
+    }, false);
+  };
+
+  dc.devopen = function () {
+    document.querySelector("#devBtn").style.display = "inline";
+  };
+
+  dc.devTog = function () {
+    $(".devMode").css("display", "block");
+    $(".col1").css("background", "crimson");
+  };
+
+  global.$dc = dc;
+})(window);
