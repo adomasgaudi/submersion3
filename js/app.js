db07402d7c9ed189219e1b9282c5331518f727ac
Adomas <adomas.gaudi@gmail.com>
2020-11-21 17:36:25 +0200

    up

--- js/app.js
+++ js/app.js
@@ -10,15 +10,48 @@ fetch("./include/footer.html")
     document.querySelector("footer").innerHTML = data;
   });
 
+  window.onload = () => {
 
-// img width
-let height = document.querySelector(".cardsrow img").offsetHeight;
-let width = height + 24;
-let text = " "+ width + "px "
-console.log(height, height+30, text)
-document.querySelectorAll(".cardsrow img").forEach(e=>{
-  e.style.width = text;
-});
+    let imgHeight = $('.cardsrow img').outerHeight();
+    document.querySelectorAll(".cardsrow img").forEach(e=>{
+      e.style.width = " "+ (imgHeight+24) + "px ";
+    });
+
+    
+
+
+
+
+
+  };
+
+  function sizeFix(){
+    let vidWidth = $('#article iframe').outerWidth();
+    let vidHeight;
+    console.log( vidWidth, vidHeight);
+    if(vidWidth > 500){
+      vidHeight = (vidWidth/16)*9;
+      console.log("16:9", vidWidth, vidHeight);
+    }else{
+      vidHeight = (vidWidth/4)*3;
+      console.log("4:3");
+    }
+    document.querySelectorAll("#article iframe").forEach(e=>{
+      e.style.height = " "+ (vidHeight) + "px ";
+    });
+  }
+
+
+
+  window.addEventListener('resize', function(){
+    
+    sizeFix()
+  });
+
+
+// (function(global){    
+
+//  })(window);
 
 
 
@@ -45,11 +78,14 @@ document.querySelectorAll(".cardsrow img").forEach(e=>{
     $ajaxUtils.sendGetRequest(
       dc.artList[article],
       function (receivedHtml) {
-        console.log(receivedHtml, "inserted HTML yay!!!!!!!!!")
+        // console.log(receivedHtml, "inserted HTML yay!!!!!!!!!")
         insertHtml("#article",receivedHtml);
 
+        sizeFix();
       },
       false);
+
+      
 
   }
 
